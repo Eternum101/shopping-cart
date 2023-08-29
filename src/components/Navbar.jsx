e20a0436f1489a635f3dc6a87530e84fa186616a
Eternum101 <eternumhd111@gmail.com>
2023-08-29 18:36:06 +1000

    Installed React Router Added components, context, data, hooks, pages, styles & utitlies folders Created Home.jsx, Store.jsx, About.jsx & Navbar.jsx Created Route Paths in App.jsx Created Index.css & Navbar.css Installed React Icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import '../styles/Navbar.css'
+import { Link } from 'react-router-dom'
+import { FaCartShopping } from 'react-icons/fa6'
+
+export function Navbar() {
+    return (
+        <header>
+            <div className='logo'>
+                <Link to="./">
+                    <h1>PartsGeek<span>.</span></h1>
+                </Link>
+            </div>
+            <div className='header-left'>
+                <Link to="./shop">Shop</Link>
+                <Link to="./about">About Us</Link>
+            </div>
+        <div className='header-right'>
+            <div className='search-input'>
+                <input className='input-search' type='text' placeholder='Search Here'></input>
+            </div>
+            <div className='cart'>
+                <Link>
+                    <FaCartShopping />
+                </Link>
+            </div>
+        </div>
+        </header>
+    )
+}
