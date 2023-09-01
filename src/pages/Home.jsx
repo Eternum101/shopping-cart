6fcf705bf2cf5686364d34c77d902b59657d6e26
Eternum101 <eternumhd111@gmail.com>
2023-09-01 18:46:22 +1000

    Modified Card Container in Home.jsx Modified Styling for Card Container in Home.css Added Promotion & Offers Container in Home.jsx Added Styling for Promotion & Offers Container in Home.css Added Trust & Latest Container in Home.jsx Added Styling for Trust & Latest Container in Home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import '../styles/Home.css'
+import { FaTruckFast, FaDollarSign, FaHeadset } from 'react-icons/fa6'
 
 export function Home() {
     return (
@@ -12,19 +13,46 @@ export function Home() {
             <button className="shop-btn">SHOP ALL</button>
         </div>
     </div>
-<div className='content-container'>
     <div className='card-container'>
-        <div className='shipping-card'>
-            <h3>Shipping</h3>
+            <div className='card-icon'>
+                <FaTruckFast/>
+            </div>
+            <div className='card-text'>
+                <h3>Free Shipping</h3>
+                <p>For Orders Over $100</p>
+            </div>
+            <div className='card-icon'>
+                <FaDollarSign/>
+            </div>
+            <div className='card-text'>
+                <h3>Customer Support</h3>
+                <p>24/7 Customer Support Avaliable</p>
+            </div>
+            <div className='card-icon'>
+                <FaHeadset/>
+            </div>
+            <div className='card-text'>
+                <h3>Flexible Payment Options</h3>
+                <p>Buy Now, Pay Later Options Avaliable</p>
+            </div>
         </div>
-        <div className='support-card'>
-            <h3>Customer Support</h3>
+    <div className='promotion-container'>
+        <div className='promotion-text'>
+            <h3>Take Care of Your Brakes - 40% Off</h3>
+            <p>For Orders Over $100</p>
         </div>
-        <div className='payment-card'>
-            <h3>Flexible Payment Options</h3>
+        <div className='promotion-image'></div>
+    </div>
+    <div className='offers-container'>
+        <div className='trust-container'>
+            <h3>Brands We Trust</h3>
+            <p>We provide over 450 of the world's highest quality automotive parts</p>
+        </div>
+        <div className='latest-container'>
+            <p>Subscribe To Our Newsletter</p>
+            <h3>Get the Latest News & Amazing Offers</h3>
         </div>
     </div>
-</div>
     </>
     )   
 }
