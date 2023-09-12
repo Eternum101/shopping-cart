1f2eb5b7b326a4ead694a0e9e1fb59a7c6a23c34
Eternum101 <eternumhd111@gmail.com>
2023-09-12 18:03:24 +1000

    Modified Layout in Navbar.jsx Added Additional Car Parts to items.json Modifed Styling in Home.css, Navbar.css, ShoppingCart.css & Store.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,32 @@
 import '../styles/Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FaCartShopping } from 'react-icons/fa6'
 import { useShoppingCart } from '../context/ShoppingCartContext'
 import { ShoppingCart } from './ShoppingCart'
 
 export function Navbar() {
     const { cartQuantity, openCart } = useShoppingCart();
+    const location = useLocation();
 
     const handleCartClick = () => {
         console.log('Cart clicked');
         openCart();
     };
 
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    navLinks.forEach((link) => {
-        link.addEventListener('click', (event) => {
-            navLinks.forEach((navLink) => {
-                navLink.classList.remove('active');
-            });
-
-            event.target.classList.add('active');
-        })
-    })
-
     return (
         <header>
             <div className='logo'>
-                <Link to="./">
+                <Link to="/">
                     <h1>PartsGeek<span>.</span></h1>
                 </Link>
             </div>
-            <div className='header-left'>
-                <Link to="./" className='nav-link'>Home</Link>
-                <Link to="./store" className='nav-link'>Shop</Link>
-                <Link to="./about" className='nav-link'>About Us</Link>
-            </div>
         <div className='header-right'>
-            <div className='search-input'>
+            <div className='header-nav'>
+            <Link to="./" className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
+                    <Link to="/store" className={location.pathname === '/store' ? 'nav-link active' : 'nav-link'}>Shop</Link>
+                    <Link to="/about" className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}>About Us</Link>
+            </div>
+        <div className='search-input'>
                 <input className='input-search' type='text' placeholder='Search Here'></input>
             </div>
             <div className='cart'>
